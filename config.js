f72c64659d222aaa8aef5d5f7841c9c0edd31b39
Riski Yanda <135003351+BotzIky@users.noreply.github.com>
2025-01-04 23:36:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
-// Deskripsi:
+// Description:
 // This file contains system prompts used by the `lib/chatgpt.js` and `lib/chatgpt2.js` files 
 // to provide context to the AI on how it should respond to the user.
-// Example: module.exports = `You are BotzAku AI, a WhatsApp Chat Bot with artificial intelligence (AI). Your job is to answer every question, listen to complaints, give advice, and help users with info.`;
+// Example: const system = `You are BotzAku AI, a WhatsApp Chat Bot with artificial intelligence (AI). Your job is to answer every question, listen to complaints, give advice, and help users with info.`;
 
-module.exports = 'INPUT_HERE';
+const system =  'good and helpful assistant';
+module.exports = { system }
